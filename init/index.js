09331449c3629eec62e0d308e9785901e7bdadae
AfshinShaik <shaikafshin298@gmail.com>
2026-09-04 20:42:15 +0530

    Edit and Delete only for owned listings

--- init/index.js
+++ init/index.js
@@ -7,6 +7,7 @@ const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 main()
   .then(() => {
     console.log("connected to DB");
+    initDB();
   })
   .catch((err) => {
     console.log(err);
@@ -18,8 +19,11 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({ ...obj , owner : ('6a98513bd0cf046119fd7fd2')}))
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
+
+  mongoose.disconnect();
 };
 
-initDB();
+//initDB();
--- routes/listing.js
+++ routes/listing.js
@@ -33,19 +33,22 @@ router.get("/new" , isLoggedIn ,  (req,res) => {
 //LIsting details
 router.get("/:id" ,wrapAsync( async (req,res) => {
     let {id} = req.params;
-    let listing=await Listing.findById(id).populate("reviews");
+
+    let listing=await Listing.findById(id).populate("reviews").populate("owner");
     if(!listing){
             req.flash("error" , " Listing does not Exist !");
             return res.redirect("/listings");
 
     }
+    console.log(listing);
     res.render("listings/details" , {listing});
 }));
 
 //Adding new listing
-router.post("/" ,validateListing, wrapAsync ( async (req,res) => {
+router.post("/" ,isLoggedIn , validateListing, wrapAsync ( async (req,res) => {
     
     let newListing=new Listing(req.body.listing);
+    newListing.owner = req.user._id;
     await newListing.save();
     req.flash("success" , " New Listing Created");
     res.redirect("/listings");
